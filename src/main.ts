f6674501cbab1b0d6b4379c205653a14c5bc0b46
Dennis O'Keeffe <hello@dennisokeeffe.com>
2024-03-24 16:43:45 +1100

    feat: update main.ts with the shebang prefix

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 import { program } from "@commander-js/extra-typings";
 import { init } from "@/cmd/init";
 import { migrate } from "@/cmd/migrate";
@@ -5,11 +7,10 @@ import { rollback } from "@/cmd/rollback";
 import { create } from "@/cmd/create";
 import { envList } from "@/cmd/env-list";
 import { envAdd } from "@/cmd/env-add";
+import packageJson from "../package.json";
 
 async function main() {
-  program
-    .version("0.0.4")
-    .description("A simple CLI tool to manage DynamoDB data migrations");
+  program.version(packageJson.version).description(packageJson.description);
 
   program.addCommand(init);
   program.addCommand(migrate);
